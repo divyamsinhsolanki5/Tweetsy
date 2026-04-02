4991b0d5fec0411b68c19b96210fe26bcb52f217
Divyamsinh Solanki <divyamsinhsolanki05@gmail.com>
2026-04-03 01:31:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tweetsy/repository/TweetRepository.kt
@@ -0,0 +1,36 @@
+package com.example.tweetsy.repository
+
+import com.example.tweetsy.api.TweetsyAPI
+import com.example.tweetsy.models.TweetListItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+class TweetRepository @Inject constructor(private val tweetsyAPI: TweetsyAPI){
+
+    private val  _categories = MutableStateFlow<List<String>>(emptyList())
+    val categories: StateFlow<List<String>>
+        get() = _categories
+
+    private val  _tweets = MutableStateFlow<List<TweetListItem>>(emptyList())
+    val tweets: StateFlow<List<TweetListItem>>
+        get() = _tweets
+
+
+    suspend fun getCategories(){
+        val response = tweetsyAPI.getCategories()
+        if (response.isSuccessful && response.body() !=null){
+            _categories.emit(response.body()!!)
+
+        }
+    }
+
+    suspend fun getTweets(category : String){
+        val response = tweetsyAPI.getTweets("tweets[?(@.category == \"$category\")]")
+        if (response.isSuccessful && response.body() !=null){
+            _tweets.emit(response.body()!!)
+
+        }
+    }
+
+}
